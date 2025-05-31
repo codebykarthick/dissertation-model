58db4c91ac2b40b83a9c2ba99115797ead48eee2
Sri Hari Karthick <sriharikarthik2641999@gmail.com>
2025-05-31 13:38:53 +0100

    Added yolo sample script in run.sh.

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -2,6 +2,8 @@
 python data_setup.py classification
 python data_setup.py yolo
 
+
+## CLASSIFICATION
 # Training scripts - Without RoI, No Oversampling and No loss weighting
 nohup python runner.py --models mobilenetv3 efficientnet shufflenet --mode train --epochs 60 --lr 1e-4 --batch 32 --workers 8 --patience 10 > out.log 2>&1 &
 python runner.py --models cnn --mode train --epochs 70 --lr 1e-3 --batch 32 --workers 8 --patience 10 
@@ -17,3 +19,7 @@ python runner.py --models cnn --mode train --epochs 70 --lr 1e-3 --batch 32 --wo
 # Testing scripts - Without RoI
 python runner.py --models mobilenetv3 --mode evaluate --file mobilenetv3_2025-05-20_09-38-24_val_0.3869.pth
 python runner.py --models cnn --mode evaluate --file cnn_2025-05-20_09-41-03_val_0.3092.pth
+
+
+## YOLO Fine Tuning
+python roi_runner.py --mode train --batch 24
