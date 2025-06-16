8a267e02f5c43759b6b6491284972400e1bc6f93
Sri Hari Karthick <sriharikarthik2641999@gmail.com>
2025-06-16 14:33:40 +0100

    Added more experiments in the next script.

--- src/scripts/next.sh
+++ src/scripts/next.sh
@@ -54,7 +54,9 @@ python data_setup.py classification
 echo "Running Siamese Few-Shot with ShuffleNet backend"
 python runner.py --task_type siamese --models shufflenet --mode train --epochs 60 --lr 1e-4 --batch 32 --workers 8 --patience 10 --label siamese_shufflenet
 
-# # Evaluation on test set
+# Evaluation on test set
+echo "Running evaluation on Test set for EfficientNet fine tuning"
+python runner.py --task_type classification --models efficientnet --mode evaluate --batch 32 --workers 8 --patience 10 --roi --weighted_sampling --label efficientnet_roi_weighted_sampling --file efficientnet_fold1_2025-06-16_13-21-33.pth
 # echo "Running evaluation on Test set for Normal fine tuning"
 # python runner.py --task_type classification --models shufflenet --mode evaluate --batch 32 --workers 8 --patience 10 --roi --weighted_sampling --label shufflenet_roi_weighted_sampling --file shufflenet_fold1_2025-06-14_11-53-58.pth
 
