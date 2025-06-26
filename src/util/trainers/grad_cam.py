a8b22f2ef5995b6032be5ca68f5630fa17afa66a
Sri Hari Karthick <sriharikarthik2641999@gmail.com>
2025-06-26 12:04:15 +0100

    Switched to activation based CAM instead of Gradient to prevent requiring layer unfreezing.

--- src/util/trainers/grad_cam.py
+++ src/util/trainers/grad_cam.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn.functional as F
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
-from torchcam.methods import SmoothGradCAMpp
+from torchcam.methods import SSCAM, SmoothGradCAMpp
 from torchcam.utils import overlay_mask
 from torchvision import transforms
 from torchvision.models import EfficientNet, ShuffleNetV2
@@ -97,43 +97,43 @@ class GradCamBench(Trainer):
             std=[1/0.229, 1/0.224, 1/0.225]
         )
 
-        # Run Smooth Grad-CAM++ with automatic hook handling
-        with torch.enable_grad():
-            with SmoothGradCAMpp(
-                self.model,
-                target_layer=self.target_layer,
-                num_samples=8,
-                std=0.2
-            ) as cam_extractor:
-                for batch_idx, (images, labels) in enumerate(tqdm(self.test_loader, desc="Grad-CAM")):
-                    images = images.to(self.device)
-                    labels = labels.to(self.device)
-                    outputs = self.model(images)
-                    probs = torch.sigmoid(outputs)
-                    preds = (probs > self.threshold).int()
+        # Run SSCAM with automatic hook handling
+        with SSCAM(
+            self.model,
+            target_layer=self.target_layer,
+            batch_size=self.batch_size,
+            num_samples=35,
+            std=2.0
+        ) as cam_extractor:
+            for batch_idx, (images, labels) in enumerate(tqdm(self.test_loader, desc="Grad-CAM")):
+                images = images.to(self.device)
+                labels = labels.to(self.device)
+                outputs = self.model(images)
+                probs = torch.sigmoid(outputs)
+                preds = (probs > self.threshold).int()
 
-                    for i in tqdm(range(images.size(0)), desc=f"Batch {batch_idx+1}", leave=False):
-                        # Single image tensor [C,H,W]
-                        image_tensor = images[i]
+                for i in tqdm(range(images.size(0)), desc=f"Sub batch process"):
+                    # Single image tensor [C,H,W]
+                    image_tensor = images[i]
 
-                        output = outputs[i].unsqueeze(0)
-                        pred_class = int(preds[i].item())
-                        label_class = int(labels[i].item())
+                    output = outputs[i].unsqueeze(0)
+                    pred_class = int(preds[i].item())
+                    label_class = int(labels[i].item())
 
-                        # Generate CAM mask for the single-logit output (always channel 0)
-                        # As it is a binary classification
-                        activation_map = cam_extractor(
-                            class_idx=0, scores=output)[0].cpu()
-                        # Directly overlay mask without interpolation
-                        unnormal_img = inv_normalize(
-                            image_tensor.cpu()).clamp(0.0, 1.0)
-                        result = overlay_mask(
-                            to_pil_image(unnormal_img),
-                            to_pil_image(activation_map[0], mode='F'),
-                            alpha=0.5
-                        )
-                        # Mark whether the prediction was correct
-                        correct = (pred_class == label_class)
-                        status = "correct" if correct else "incorrect"
-                        fname = f"img_{batch_idx}_{i}_pred_{pred_class}_label_{label_class}_{status}_score{probs[i].item():.2f}.png"
-                        result.save(os.path.join(save_root, fname))
+                    # Generate CAM mask for the single-logit output (always channel 0)
+                    # As it is a binary classification
+                    activation_map = cam_extractor(
+                        class_idx=0, scores=output)[0].cpu()
+                    # Directly overlay mask without interpolation
+                    unnormal_img = inv_normalize(
+                        image_tensor.cpu()).clamp(0.0, 1.0)
+                    result = overlay_mask(
+                        to_pil_image(unnormal_img),
+                        to_pil_image(activation_map[0], mode='F'),
+                        alpha=0.5
+                    )
+                    # Mark whether the prediction was correct
+                    correct = (pred_class == label_class)
+                    status = "correct" if correct else "incorrect"
+                    fname = f"img_{batch_idx}_{i}_pred_{pred_class}_label_{label_class}_{status}_score{probs[i].item():.2f}.png"
+                    result.save(os.path.join(save_root, fname))
